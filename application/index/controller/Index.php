77829f93c3da930b39837d2f9062ee2a6104faef
xmosnewone <8868814+xmosnewone@user.noreply.gitee.com>
2024-10-06 14:20:46 +0800

    删除Index模块测试输出表结构代码

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,19 +8,5 @@ class Index extends Controller
     public function index()
     {
 
-        $list = Db::query('show table status');
-        foreach($list as $value){
-                $name=$value['Name'];
-                $comment=$value['Comment'];
-                $fields=Db::query('SHOW FULL COLUMNS FROM `' . $name . '`');
-                echo $name.":".$comment."<br>";
-                foreach($fields as $fval){
-                  echo  "Field Name:".$fval['Field'];
-                  echo "<br>Field Comment:".$fval['Comment'];
-                  echo  "<br>Field Type:".$fval['Type'];
-                    echo  "<br>----------------<br>";
-                }
-                echo '##############################################################';
-        }
    	}
 }
